e3c3b2b1362ec9cc0260e9c8b4ee4c575682caa6
m3ltman <m3ltman@gmail.com>
2020-03-05 11:35:35 +0500

    Fix handling of unknown routes in router and add exception to eslint in centralized error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
-const errorHandler = (err, req, res, next) => {
+/* eslint-disable no-unused-vars */
+const errorHandler = (err, _req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'Internal Server Error' : message });
 };
