79ab8695441e15a0e878eeede49b71d9fa8cf3a6
m3ltman <m3ltman@gmail.com>
2020-03-10 15:02:26 +0500

    Add RegExp into config file

--- configuration/config.js
+++ configuration/config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-control-regex */
 require('dotenv').config();
 
 const { NODE_ENV, JWT_SECRET, PORT = 3000 } = process.env;
@@ -9,6 +11,7 @@ const dbOptions = {
   useUnifiedTopology: true,
 };
 const key = NODE_ENV !== 'production' ? 'dev_secret' : JWT_SECRET;
+const UrlRegExp = new RegExp('^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$');
 
 module.exports = {
   PORT,
@@ -17,4 +20,5 @@ module.exports = {
   NODE_ENV,
   JWT_SECRET,
   key,
+  UrlRegExp,
 };
